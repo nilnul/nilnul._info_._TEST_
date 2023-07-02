434dc27f5dddee0a6f3ac96595a279b73abe53ef
WANG,Youtian <wangyoutian@nilnul.com>
2023-07-02 14:42:32 +0800

    start the repo;

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("nilnul._info_._TEST_")]
+[assembly: AssemblyDescription("nilnul._info_._TEST_")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("nilnul.com")]
+[assembly: AssemblyProduct("nilnul._info_._TEST_")]
+[assembly: AssemblyCopyright("wangyoutian@nilnul.com")]
+[assembly: AssemblyTrademark("nilnul._info_._TEST_")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0a660127-0fcc-416c-92e6-548f4972a68a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ nilnul0/stat/dist_/finite/occurrence/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace nilnul._stat_._TEST_.dist_.finite_.occurrence
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+			var t = nilnul.stat.dist_.finite._EntropyX.Entropy_ofAssumeOccurrences(
+				5,9
+			);
+			Debug.WriteLine(t);
+		}
+	}
+}
